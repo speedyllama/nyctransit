ed9997e2496c16f582e7b1218f4cd1b6bdc6b643
Mike Zou <mzou@speedyllama.com>
2015-12-06 00:25:37 -0500

    Try a different servlet init method

--- src/main/java/com/speedyllama/mtastatus/MTAStatusServlet.java
+++ src/main/java/com/speedyllama/mtastatus/MTAStatusServlet.java
@@ -7,17 +7,10 @@ import com.amazon.speech.speechlet.servlet.SpeechletServlet;
 
 public class MTAStatusServlet extends SpeechletServlet {
 	private static final long serialVersionUID = 1892350072140640799L;
-	private MTAStatusSpeechlet speechlet;
 	
 	@Override
 	public void init() throws ServletException {
-		this.speechlet = new MTAStatusSpeechlet();
+		setSpeechlet(new MTAStatusSpeechlet());
 		super.init();
 	}
-
-	@Override
-	public Speechlet getSpeechlet() {
-		return this.speechlet;
-	}
-
 }
