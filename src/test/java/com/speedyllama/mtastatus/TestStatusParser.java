dfe27b9440656880983e3673af5033306b473045
Mike Zou <mzou@speedyllama.com>
2015-12-17 23:49:17 -0500

    First test done

--- src/test/java/com/speedyllama/mtastatus/TestStatusParser.java
+++ src/test/java/com/speedyllama/mtastatus/TestStatusParser.java
@@ -1,5 +1,7 @@
 package com.speedyllama.mtastatus;
 
+import static org.junit.Assert.*;
+
 import java.util.Map;
 
 import org.junit.Before;
@@ -15,9 +17,25 @@ public class TestStatusParser {
 		this.fetcher = new PlainXMLFetcher();
 	}
 
+	private void assertGoodService(String trains, Map<String, Status> statusMap) {
+		for (String train : trains.split("\\|")) {
+			assertSame(statusMap.get(train).getStatus(), TrainStatus.GOOD_SERVICE);
+		}
+	}
+	
 	@Test
 	public void test() throws MTAStatusException {
 		Map<String, Status> statusMap = parser.parse(fetcher.fetchXML(getClass().getResource("/xmls/20151217T2257.xml").toString()));
-		assert(statusMap.get("1").getStatus() == TrainStatus.GOOD_SERVICE);
+		assertGoodService("1|2|3|4|5|6|7|A|C|B|J|Z|L|N|Q|R|S|SIR", statusMap);
+		assertSame(statusMap.get("E").getStatus(), TrainStatus.PLANNED_WORK);
+		// Two alerts combined
+		assertEquals(statusMap.get("E").getTitle(), "[E] Jamaica Center-bound trains run local from Queens Plaza to 71 Av. [E] World Trade Center-bound trains run local from 71 Av to Queens Plaza. ");
+		assertSame(statusMap.get("D").getStatus(), TrainStatus.DELAYS);
+		assertEquals(statusMap.get("D").getTitle(), "Due to ongoing signal problems at Bay 50 St, southbound [D] trains are running with delays. Allow additional travel time.. ");
+		assertSame(statusMap.get("F").getStatus(), TrainStatus.PLANNED_WORK);
+		assertEquals(statusMap.get("F").getTitle(), "[F] Trains run local in both directions between 21 St-Queensbridge and 71 Av. ");
+		assertSame(statusMap.get("M").getStatus(), TrainStatus.PLANNED_WORK);
+		assertSame(statusMap.get("G").getStatus(), TrainStatus.PLANNED_WORK);
+		assertEquals(statusMap.get("G").getTitle(), "[G] No trains between Bedford-Nostrand Avs and Court Sq. ");
 	}
 }
